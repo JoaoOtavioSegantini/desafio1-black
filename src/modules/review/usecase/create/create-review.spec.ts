9501bafff609ff877a21018bea26136affb8e4c2
João Otávio <joaotavioos@outlook.com>
2022-06-03 12:40:01 -0300

    test: add create review usecase unit test 🚨

---
+++ src/modules/review/usecase/create/create-review.spec.ts
@@ -0,0 +1,36 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import CreateReviewUseCase from "./create-review.usecase";
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    find: jest.fn(),
+    findAll: jest.fn(),
+    delete: jest.fn(),
+    getReviews: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Create review usecase unit test", () => {
+  it("should add a new review", async () => {
+    const reviewRepository = MockRepository();
+    const usecase = new CreateReviewUseCase(reviewRepository);
+
+    const input = {
+      clientId: 2,
+      restaurantId: 1,
+      stars: 1,
+      comment: "Ótimo",
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(reviewRepository.create).toHaveBeenCalled();
+    expect(result.id).toBeDefined;
+    expect(result.clientId).toBe(input.clientId);
+    expect(result.restaurantId).toBe(input.restaurantId);
+    expect(result.stars).toBe(input.stars);
+    expect(result.comment).toBe(input.comment);
+  });
+});
