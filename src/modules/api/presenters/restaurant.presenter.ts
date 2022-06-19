b55b752166a70d719081579551a54dd6d1c042d0
João Otávio <joaotavioos@outlook.com>
2022-06-19 18:08:40 -0300

    feat: add restaurant presenter ✨

---
+++ src/modules/api/presenters/restaurant.presenter.ts
@@ -0,0 +1,26 @@
+import { toXML } from "jstoxml";
+import { FindAllRestaurantOutputDto } from "../../restaurant/usecase/retrieve/findAll/findAll-restaurant.dto";
+
+export default class RestaurantPresenter {
+  static listXML(data: FindAllRestaurantOutputDto): string {
+    const xmlOption = {
+      header: true,
+      indent: "  ",
+      newline: "\n",
+      allowEmpty: true,
+    };
+
+    return toXML(
+      {
+        restaurant: data.map((res) => ({
+          id: res.id,
+          name: res.name,
+          phone: res.phone,
+          description: res.description,
+          address: res.address,
+        })),
+      },
+      xmlOption
+    );
+  }
+}
